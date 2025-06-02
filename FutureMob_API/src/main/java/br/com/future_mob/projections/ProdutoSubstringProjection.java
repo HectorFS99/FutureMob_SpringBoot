81df87544ddea7aae9ab23c84b917622bc1e43df
Hector Ferreira da Silva <fhector7@gmail.com>
2025-06-02 20:28:31 -0300

    Reorganização de arquivos

--- FutureMob_API/src/main/java/br/com/future_mob/projections/ProdutoSubstringProjection.java
+++
@@ -1,7 +0,0 @@
-package br.com.future_mob.projections;
-
-public interface ProdutoSubstringProjection {
-	public String getNome_produto();
-	public String getNome_categoria();
-	public Float getPrecoAnterior();
-}
